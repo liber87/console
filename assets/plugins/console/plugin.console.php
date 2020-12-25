aeb1e820a75f7a15133d1473136dad892acd592e
liber87 <bd87@bk.ru>
2020-12-25 06:02:13 +0300

    Update plugin.console.php
    Добавил проверку на modx.popup и modx.tabs, $e-> - $modx->event

--- assets/plugins/console/plugin.console.php
+++ assets/plugins/console/plugin.console.php
@@ -1,19 +1,8 @@
 <?php
 if ($_SESSION['mgrRole'] != 1) return;
-$e = &$modx->event; 
-switch ($e->name)
-{
-    case 'OnManagerTreeRender':
-    if ($modal=='yes')
-    $SCRIPT = <<<console
-var icon = '<a class="treeButton"  onclick="window.open(\'../assets/plugins/console/console.php\',\'gener\',\'width=800,height=560,top=\'+((screen.height-600)/2)+\',left=\'+((screen.width-800)/2)+\',toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=no\')" title="Console"><i class="fa fa-terminal fa-lg"></i></a>';
-console;
-    
-    if ($modal=='no')
-    $SCRIPT = <<<console
-var icon = '<a class="treeButton" onclick="top.main.location.href=\'../assets/plugins/console/console.php\'" title="Консоль"><i class="fa fa-terminal fa-lg"></i></a>';
-console;
-    $SCRIPT .= <<<console
+
+$SCRIPT = <<<console
+var icon = '<a class="treeButton" title="Консоль" id="consoleButton"><i class="fa fa-terminal fa-lg"></i></a>';
 var menu = document.getElementById('treeMenu');
 var el = document.createElement('td');
 el.innerHTML = icon;
@@ -22,7 +11,31 @@ if (menu.tagName === 'TABLE') {
 } else {
     menu.appendChild(el.firstChild);
 }
+var cb = document.getElementById('consoleButton');
 console;
-    $e->output('<script>'.$SCRIPT.'</script>');
-    break;
+
+if ($modal=='yes')
+$SCRIPT .= <<<console
+if (typeof modx.popup == 'function') {
+	cb.addEventListener("click", function(){
+		modx.popup({url: '../assets/plugins/console/console.php',icon: 'fa-code',title: 'Консоль', draggable: true,'width': '90%','height':'90%','hide': 0,'hover': 0});
+	});	
+} else {
+	cb.addEventListener("click", function(){window.open('../assets/plugins/console/console.php','gener','width=800,height=560,top='+((screen.height-600)/2)+',left='+((screen.width-800)/2)+',toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=no');
+	});		
 }
+console;
+
+if ($modal=='no')
+$SCRIPT .= <<<console
+if (typeof modx.tabs == 'function') {
+	cb.addEventListener("click", function(){
+		modx.tabs({url: '../assets/plugins/console/console.php', title: 'Консоль'});
+	});	
+} else {
+	cb.addEventListener("click", function(){
+		top.main.location.href='../assets/plugins/console/console.php'
+	});		
+}
+console;
+$modx->event->setOutput('<script>'.$SCRIPT.'</script>');    
